6d4acfd9cf05a3ec34236ded8300b6e2108cc5a5
0vos <liqianqian1872@gmail.com>
2024-12-24 13:03:23 +0800

    Update statistic.cpp

--- statistic.cpp
+++ statistic.cpp
@@ -1,5 +1,7 @@
 #include <unordered_map>
 #include <iostream>
+#include <sstream>
+#include <iomanip>
 #include <string>
 using namespace std;
 
@@ -21,8 +23,14 @@ string get_statistc(string* trees, int length){
 StaForest::StaForest(string *trees, int length){
     size = 0;
     for(size_t i=0;i<length;++i){
-        size_t end_pos = trees[i].find(",", 0);
-        string name = trees[i].substr(0, end_pos - 0);
+        size_t end_pos = trees[i].find(",", 1);
+        string name;
+        if(i==0){
+            name = trees[i].substr(0, end_pos);
+        }
+        else{
+            name = trees[i].substr(1, end_pos - 1);
+        }
         if(!name.empty()){
             size++;
             if(forest.count(name)){
@@ -41,23 +49,51 @@ StaForest::StaForest(string *trees, int length){
 
 string StaForest::statistic(){
     string result = "";
+    // cout << "size: " << size << endl;
+    // for (auto it = forest.begin(); it != forest.end(); ++it) {
+    //     cout << "Tree type: " << it->first << ", Count: " << it->second << endl;
+    // }
     for(auto it=forest.begin();it!=forest.end();++it){
         result += it->first;
         if(size){
             // cout << "name: " << it->first << "number: " << it->second << endl;
             double percent = 100 * (double(it->second) / double(size));
-            result += " " + to_string(percent) + "%";
+            result += " " + to_string(percent) + "%\n";
         }
         else{
             printf("树木总数为空！\n");
             exit(0);
         }
     }
-    result = result.substr(1);
-    size_t pos0 = result.rfind("%");
-    size_t pos1 = result.rfind("%", pos0 - 1);
-    result.insert(pos1+1, "\n");
     cout << "result\n" << result << endl;
+    string hint = "<----------树种统计程序---------->\n1. 查找一棵树\n2. 增加一棵树\n3. 删除一棵树\n4. 退出\n请输入功能对应的序号进行操作: ";
+    cout << hint << endl;
+    return result;
+}
+
+string statistic_string2json(const string& input) {
+    istringstream stream(input);
+    string line, key, value, result = "{";
+
+    while (std::getline(stream, line)) {
+        istringstream lineStream(line);
+
+        // 分离 key 和 value
+        lineStream >> key >> value;
+
+        // 去掉 '%' 符号
+        if (!value.empty() && value.back() == '%') {
+            value.pop_back();
+        }
+
+        // 添加到 JSON 格式字符串
+        if (result.size() > 1) {  // 如果不是第一个元素，添加逗号分隔
+            result += ",";
+        }
+        result += "\"" + key + "\":\"" + value + "\"";
+    }
+
+    result += "}";
     return result;
 }
 
