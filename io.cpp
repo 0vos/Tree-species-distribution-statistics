38af3bc7b5b51743a5e7cb7c83e22b73ae12afc4
mark-liusi <136895645+mark-liusi@users.noreply.github.com>
2024-12-26 15:00:40 +0800

    中文和search

--- io.cpp
+++ io.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 #include<fstream>
 #include<string>
-// #include"statistic.cpp"
+#include"statistic.cpp"
 #include <locale>
+#include <windows.h>
+#include"human_desire.cpp"
 using namespace std;
 
 int count(string filename){
@@ -22,7 +24,7 @@ int count(string filename){
     return number_of_trees/2;
 }
 void add(string filename){
-    // cout<<"ä¸Šæ ‘,(99,99)"<<endl;
+    cout<<"ÉÏÊ÷,(99,99)"<<endl;
     string addition= "";
     cin>>addition;
     string content= "";
@@ -31,7 +33,7 @@ void add(string filename){
     while(ifile.get(c)){
         content+= c;
     }
-    //content= content+'\n'+ addition;
+    content= content+'\n'+ addition;
     //cout<<content;
      
     ofstream ofile(filename);
@@ -52,125 +54,119 @@ void read(string filename, string *p)
     }
     int i, j=0;
     for(i=0; i<content.size(); i++){
-        if(content[i]!= ')'){
+        if(content[i]!= '\n'){
             p[j]+= content[i];
         }else{
-            p[j]+= ')';
             j++;
         }
     }
     
 }
-int repeat(int i, int j, int next[], string in_test){
-    //repeatå‡½æ•°ä¸»è¦ç”¨æ¥é€’å½’è®¡ç®—nextæ•°ç»„ï¼Œå°±æ˜¯é‡‡ç”¨æœ€å¸¸è§„çš„nextæ•°ç»„çš„è®¡ç®—æ–¹æ³•
-    if(i== 0){
-        return 0;
-    }else{
-        i--;
-        i= next[i];
-        if(in_test[i]== in_test[j]){
-            return i;
-        }else{
-            i= repeat(i, j, next, in_test);
-            return i;
-        }
-    }
-}
-string search(string test, string in_test){
-    int next[in_test.size()];
-    for(int i=0;i<in_test.size();++i){
-        next[i] = 0;
-    }
-    int i=0, j=1;
-    //è®¡ç®—nextæ•°ç»„
-    while(j<= in_test.size()){
-        if(in_test[j]== in_test[i]){
-            next[j]= i+1;
-            i++;
-            j++;
-        }else{
-            if(i== 0){
-                j++;
-            }else{
-                i= repeat(i, j, next, in_test);
-            }
-        }
-    }
-    i=0, j=0;
-    int judge= 0;
-    string posion= "";
-    while(j<= test.size()){
-        if(i== in_test.size()){
-            judge++;
-            posion+= to_string(j-in_test.size());
-            posion+="_";
-            i= 0;
-        }else{
-            if(test[j]== in_test[i]){
-                i++;
-                j++;
-            }else{
-                if(i== 0){
-                    j++;
-                }else{
-                    i= next[i-1];
-                    if(test[j]== in_test[i]){
-                        i++;
-                    }
-                    j++;
-                }
-            }
-        }
-    }
-    posion+= to_string(judge);
-    return posion;
-}
 void remove_info(string filename){
-    cout<<"è¯·è¾“å…¥ä½ æƒ³è¦åˆ é™¤çš„æ•°æ®"<<endl;
+    cout<<"ÇëÊäÈëÄãÏëÒªÉ¾³ıµÄÊ÷ÖÖ"<<endl;
     ifstream file(filename);
     char c;
     string content= "";
     while(file.get(c)){
         content+= c;
     }
-    string re_move= "æ¢§æ¡æ ‘,(40,55)";
-    re_move= re_move+'\n';
-    //cin>>re_move;
+    //½ØÖ¹µ½ÕâÀïÊÇ°ÑÎÄ¼şÄÚÈİÂ¼Èëµ½contentÖĞ
+    string re_move;
+    cin>>re_move;//ÊäÈëÒª×÷ÎªÉ¾³ıµÄ×Ö·û´®
+    //ÕâÀïÅĞ¶¨re_moveµÄÊäÈë¿ÉÒÔÖ±½ÓÍË³öÑ­»·
     string position= search(content, re_move);
-    int i= 0, j= 0, k= 0;
-    string position_of_same= "";
-    while(position[i]!= '\0'){
-        if(position[i]== '_'){
-            break;
-        }else{
-            position_of_same+= position[i];
-        }
-        i++;
+    int number_of_same_tree= count_num_of_same(content, re_move);//¶ÔÓÚÏàÍ¬µÄÄÚÈİ½øĞĞ¼ÆÊı£¬±ÈÈçËµËÉÊ÷Ò»¹²ÓĞ¶àÉÙ×ø±ê
+
+    while(number_of_same_tree== 0){//Òì³£ÊäÈë´¦Àí£¬½øĞĞÌáÊ¾£¬¿ÉÒÔÔÚÕâÀï¼ÓÈë±ÈÈçexit¾ÍÍË³ö
+        cout<<"error input,ÇëÖØĞÂÊäÈë"<<endl;
+        cin>>re_move;
+        position= search(content, re_move);
+        number_of_same_tree= count_num_of_same(content, re_move);
     }
-    int number= stoi(position_of_same);
-    string part1= content.substr(0, number);
-    string part2= content.substr(number+re_move.size());
+    int *p= new int[number_of_same_tree];//Êı×ép°üº¬ÁË¸ÃËÑË÷ÄÚÈİËùÓĞÎ»ÖÃ£¬×¼±¸½øĞĞÉ¾¸Ä
+    posion_of_number(p, content, re_move);
+    
+    int i;
+    for(i= 0; i<number_of_same_tree; i++){
+        int number= p[i];
+        int end= content.find('\n', p[i]);//Ò»Ö±ÕÒµ½ÕâÒ»ĞĞµÄÄ©Î²£¬ÍêÕûµÄÊä³öÕâÒ»ĞĞ
+        string part= content.substr(number, end- number);
+        cout<<i+1<<"."<<part<<endl;
+    }
+    cout<<"ÇëÊäÈëÄãÏëÉ¾³ıµÄÎ»ÖÃ"<<endl;
+    string po;
+    int judge= 0;
+    while(judge== 0){//Òì³£´¦Àí
+        cin>>po;
+        for(int i=0; i<number_of_same_tree; i++){
+            if(po!= to_string(i+1)){
+                continue;
+            }else{
+                judge= 1;
+                break;
+            }
+        }
+        if(judge== 0){
+            cout<<"error input"<<endl;
+        }
+        
+    }
+    int number= p[stoi(po)-1];
+    string part1= content.substr(0, number);//·ìºÏÁ½¸ö×Ö·û´®
+    int end= content.find('\n', number);
+    string part2= content.substr(end+1);
     content= part1+part2;
     //cout<<content;
     ofstream ofile(filename);
     ofile<<content;
     //return position;
 }
-// int main(){
-//     string filename= "tree_info.txt";
-//     //string *p= new string[count(filename)];
-//     string name;
-//     //name= "æ¾æ ‘,(75,92)";
-//     remove(filename);
-//     // scanf("%s", name);
-//     // cout<<name;
-//     // read(filename, p);
-//     // add(filename);
-//     //delete[]p;
-//     //string final= get_statistc(p, count(filename));
-//     //cout<<final<<endl;
-//     // for(int i=0; i<count(filename); i++){
-//     //     cout<<p[i];
-//     // }
-//     return 0;
-// }
+void search_trees(string filename){
+    cout<<"ÇëÊäÈëÄãÏëÒª²éÑ¯µÄÊ÷ÖÖ"<<endl;
+    ifstream file(filename);
+    char c;
+    string content= "";
+    while(file.get(c)){
+        content+= c;
+    }
+    string re_move;
+    cin>>re_move;
+    //ÕâÀïÅĞ¶¨re_moveµÄÊäÈë¿ÉÒÔÖ±½ÓÍË³öÑ­»·
+
+
+    string position= search(content, re_move);
+    int number_of_same_tree= count_num_of_same(content, re_move);
+    while(number_of_same_tree== 0){
+        cout<<"²»´æÔÚ,ÇëÖØĞÂÊäÈë"<<endl;
+        cin>>re_move;
+        position= search(content, re_move);
+        number_of_same_tree= count_num_of_same(content, re_move);
+    }
+    int *p= new int[number_of_same_tree];
+    posion_of_number(p, content, re_move);
+    
+    for(int i= 0; i<number_of_same_tree; i++){
+        int number= p[i];
+        int end= content.find('\n', p[i]);
+        string part= content.substr(number, end- number);
+        cout<<i+1<<"."<<part<<endl;
+    }
+}
+int main(){
+    string filename= "tree_info.txt";
+    string *p= new string[count(filename)];
+    // string name;
+    // cin>>name;
+    //name= "æ¾æ ‘,(75,92)";
+    //search_trees(filename);
+    remove_info(filename);
+    //read(filename, p);
+    //add(filename);
+    //delete[]p;
+    //string final= get_statistc(p, count(filename));
+    //cout<<final<<endl;
+    // for(int i=0; i<count(filename); i++){
+    //     cout<<p[i]<<endl;
+    // }
+    return 0;
+}
